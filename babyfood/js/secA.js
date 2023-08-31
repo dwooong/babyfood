a9c9e0906901e26e38d7a04a844ff618425b8b15
dwooong <140695010+dwooong@users.noreply.github.com>
2023-08-31 22:17:01 +0900

    secA basic, secB not yet

--- babyfood/js/secA.js
+++ babyfood/js/secA.js
@@ -2,29 +2,9 @@ const categories = ['한식', '중식'];
 let categoryMenus = {};
 
 // 같은 폴더 내 JSON 파일을 이용할 경우
-// Function to fetch menu data from JSON file
-// async function getMenuData() {
-//   try {
-//     const response = await fetch('menuData.json');
-//     if (!response.ok) {
-//       throw new Error(`Failed to fetch menu data (HTTP status: ${response.status}).`);
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     alert('Error fetching menu data.');
-//     console.error(error);
-//     return {};
-//   }
-// }
-
-
-// Update the URL to point to the external JSON file
-const externalJsonUrl = 'https://gist.githubusercontent.com/dwooong/ea2c673165c2dc54767228bd012b7173/raw/c04e7321ef9955a361593808426ebb86f252e885/menuData.json';
-
-// Function to fetch menu data from JSON file
 async function getMenuData() {
   try {
-    const response = await fetch(externalJsonUrl); // Use the external URL
+    const response = await fetch('js/dataA.json');
     if (!response.ok) {
       throw new Error(`Failed to fetch menu data (HTTP status: ${response.status}).`);
     }
@@ -35,6 +15,25 @@ async function getMenuData() {
     return {};
   }
 }
+
+
+// Update the URL to point to the external JSON file
+// const externalJsonUrl = 'https://gist.githubusercontent.com/dwooong/ea2c673165c2dc54767228bd012b7173/raw/c04e7321ef9955a361593808426ebb86f252e885/menuData.json';
+
+// Function to fetch menu data from JSON file
+// async function getMenuData() {
+//   try {
+//     const response = await fetch(externalJsonUrl); // Use the external URL
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch menu data (HTTP status: ${response.status}).`);
+//     }
+//     return await response.json();
+//   } catch (error) {
+//     alert('Error fetching menu data.');
+//     console.error(error);
+//     return {};
+//   }
+// }
 
 
 let selectedLanguage = 'Korean'; // Default language is Korean
@@ -50,14 +49,18 @@ async function selectCategory(category) {
   const foodImagesRoulette = document.querySelector('.secAmenuDisplay > ul');
   foodImagesRoulette.textContent = '';
 
-  const buttons = document.querySelectorAll('.category-btn');
-  buttons.forEach((button) => {
-    if (button.innerText === category) {
-      button.classList.add('active');
-    } else {
-      button.classList.remove('active');
-    }
-  });
+  const secAdetailImagelist = document.querySelector('.secAdetailImage > ul');
+  secAdetailImagelist.textContent = '';
+
+  const secAmainIngredientlist = document.querySelector('.secAmainIngredient > ul');
+  secAmainIngredientlist.textContent = '';
+
+  const secAsubIngredientlist = document.querySelector('.secAsubIngredient > ul');
+  secAsubIngredientlist.textContent = '';
+
+  const secAseasoninglist = document.querySelector('.secAseasoning > ul');
+  secAseasoninglist.textContent = '';
+
 
 
   const menuData = await getMenuData();
@@ -75,9 +78,20 @@ async function selectCategory(category) {
     // Display the menu names based on the selected language
     const name = selectedLanguage === 'Korean' ? menu.nameKorean : menu.nameEnglish;
     const url = menu.url; // Get the URL from the menu data
-    foodImagesRoulette.innerHTML += `<li><a href="${url}" target="_blank"><img src="${menu.image}" alt="${name}" /><div><img src="instagram_logo.png" alt="Instagram" class="insta_logo"/><div class="menu-pan">${name}</div></div></a></li>`;
+    foodImagesRoulette.innerHTML += `<li><button><img class="foodImage" src="${menu.image}" alt="${name}" /><div class="menu-pan-wrapper"><div class="menu-pan">${name}</div></div></button></li>`;
+    
+    secAdetailImagelist.innerHTML += `<a href="${url}" target="_blank"><img class="menu-pan-logo" src="image/instagram_logo.png" alt="Instagram" class="insta_logo"/></a><li><img class="foodImage" src="${menu.img1}" alt="${name}" /></li>`;
+    
+    secAmainIngredientlist.innerHTML += `주재료 : <a href="${url}"></a><li>${menu.mainIngredient}</li>`;
+    secAsubIngredientlist.innerHTML += `부재료 : <a href="${url}"></a><li>${menu.subIngredient}</li>`;
+    secAseasoninglist.innerHTML += `양념 : <a href="${url}"></a><li>${menu.seasoning}</li>`;
   }
 }
+
+// 클릭된 메뉴에 대한 상세 이미지와 재료 표시
+// function detailMark(){
+
+// }
 
 // Initialize the app when the page is loaded
 document.addEventListener('DOMContentLoaded', initApp);
--- babyfood/js/section.js
+++ babyfood/js/section.js
@@ -3,12 +3,14 @@ const sectionBButton = document.getElementById('secBbutton');
 const sectionA = document.getElementById('sectionA');
 const sectionB = document.getElementById('sectionB');
 
+sectionB.style.display = 'none';
+
 sectionAButton.addEventListener('click', () => {
-    sectionA.style.display = 'block';
+    sectionA.style.display = 'flex';
     sectionB.style.display = 'none';
   });
   
   sectionBButton.addEventListener('click', () => {
     sectionA.style.display = 'none';
-    sectionB.style.display = 'block';
+    sectionB.style.display = 'flex';
   });
